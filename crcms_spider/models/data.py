688e9bc2698c4ca54f5b6910bd00091f3b3d82a2
crcms <crcms@crcms.cn>
2018-05-30 17:32:39 +0800

    orm test

--- crcms_spider/models/data.py
+++ crcms_spider/models/data.py
@@ -7,10 +7,31 @@
 '''
 
 from datetime import datetime
-from crcms_spider.base.manager import AbstractUrl
-from mongoengine import connect
+# from crcms_spider.base.manager import AbstractUrl
+from mongoengine import *
+
+connect('connect', host='mongodb://192.168.150.142:27017/reptile')
 
 
-connect(host = 'mongodb://192.168.150.142:27017')
+class Data(Document):
+    title = StringField(max_length=200)
+    created_at = IntField(default=int(datetime.now().timestamp()))
+    updated_at = IntField(default=int(datetime.now().timestamp()))
+    url = StringField()
+    status = IntField()#min_value=0, max_value=2, default=1
+    content = StringField()
 
-# class Data(AbstractUrl,Document):
+
+
+    def check_need_update(self, model: 'AbstractUrl', timestamp: int) -> bool:
+        pass
+
+
+    def get_by_url(self, url: str) -> dict or None:
+        pass
+
+
+
+if __name__ == '__main__':
+    data = Data(title='xxx',content='yyy',url='abc')
+    print(data.save())
